19b62318bf1f9774050acdc477a6e30f656b33ca
CsaB <b.f.csabesz@gmail.com>
2023-03-22 18:41:21 +0200

    cant think of a useful comment

---
+++ statements/createstatement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace tbd.statements
+{
+    public enum CreateType
+    {
+        Table,
+        Index,
+    }
+
+    [Serializable]
+    public class CreateStatement : IStatement
+    {
+        public StatementType Type { get; set; } = StatementType.CreateStatement;
+        public CreateType CreateType { get; set; }
+        public string TableName { get; set; }
+        // create index only
+        public bool IsUnique { get; set; } = false;
+        public string IndexName { get; set; }
+        public string AttributeName { get; set; }
+        // create table only
+        // it is assigned to "" if PrimaryKey does not exist
+        private string primaryKey = "";
+        public string PrimaryKey
+        {
+            get { return primaryKey; }
+            set
+            {
+                if (this.AttributeDeclarations.Find(x => x.AttributeName == value) == null)
+                    throw new System.Exception($"Primary key {value} does not exist");
+                primaryKey = value;
+            }
+        }
+        public List<AttributeDeclaration> AttributeDeclarations { get; set; }
+        // for serialization
+        public CreateStatement() { }
+    }
+}
---
+++ statements/deletestatement.cs
@@ -0,0 +1,10 @@
+namespace tbd.statements
+{
+    public class DeleteStatement : IStatement
+    {
+        public StatementType Type { get; set; } = StatementType.DeleteStatement;
+        public string TableName { get; set; }
+        // if no condition, it is set to null
+        public Expression Condition { get; set; } = null;
+    }
+}

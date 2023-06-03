3ec72af4bcc139c6c80b66d8e95daab2fb4406fc
CsaB <b.f.csabesz@gmail.com>
2023-06-03 18:52:11 +0300

    bugged catalogManager

--- abkrServer/CatalogManager/CatalogManager.cs
+++ abkrServer/CatalogManager/CatalogManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Xml.Linq;
+using abkr.ServerLogger;
+using abkrServer.CatalogManager.RecordManager;
 
 namespace abkr.CatalogManager
 {
     public class CatalogManager
     {
         private readonly string _metadataFilePath;
+        private Logger logger;
 
-        public CatalogManager(string metadataFilePath)
+        public CatalogManager(string metadataFilePath, Logger l)
         {
+            logger = l;
             _metadataFilePath = metadataFilePath;
         }
 
@@ -15,7 +19,7 @@ namespace abkr.CatalogManager
         {
             if (!File.Exists(_metadataFilePath))
             {
-                Console.WriteLine($"Metadata file not found at {_metadataFilePath}. Creating a new file.");
+                logger.LogMessage($"Metadata file not found at {_metadataFilePath}. Creating a new file.");
                 var databasesElement = new XElement("Databases");
                 databasesElement.Save(_metadataFilePath);
             }
@@ -26,7 +30,7 @@ namespace abkr.CatalogManager
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error reading metadata file: {ex.Message}");
+                logger.LogMessage($"Error reading metadata file: {ex.Message}");
                 return new XElement("Databases");
             }
         }
@@ -35,7 +39,7 @@ namespace abkr.CatalogManager
         {
             Directory.CreateDirectory(Path.GetDirectoryName(_metadataFilePath));
             metadata.Save(_metadataFilePath);
-            Console.WriteLine($"Metadata saved to {_metadataFilePath}");
+            logger.LogMessage($"Metadata saved to {_metadataFilePath}");
         }
 
         public List<Index> GetIndexes(string databaseName, string tableName)
@@ -83,15 +87,38 @@ namespace abkr.CatalogManager
                     if (isForeignKey)
                     {
                         var attributeName = attributeElement.Attribute("attributeName")?.Value;
+
+                        logger.LogMessage($"Found foreign key {attributeName} in table {tableName}");
+
                         var references = attributeElement.Attribute("references")?.Value;
+
+                        logger.LogMessage($"References: {references}");
+
                         var isUnique = bool.Parse(attributeElement.Attribute("isUnique")?.Value ?? "false");
+
+                        logger.LogMessage($"Is unique: {isUnique}");
+
                         if (!string.IsNullOrEmpty(attributeName) && !string.IsNullOrEmpty(references))
                         {
                             // Assumes the references string is in format "referencedTable:referencedColumn"
-                            var splitReferences = references.Split(':');
+                            var splitReferences = references.Split('(');
                             if (splitReferences.Length == 2)
                             {
-                                var foreignKey = new ForeignKey(tableName, attributeName, splitReferences[0], splitReferences[1], isUnique);
+                                var referencedTable = splitReferences[0].Split('.')[1];
+                                var referencedColumn = splitReferences[1].Split(')')[0];
+                                var pk = GetPrimaryKeyColumn(databaseName, referencedTable);
+                                bool ispk = false;
+                                if(pk == referencedColumn)
+                                {
+                                    ispk= true;
+                                }
+                                else
+                                {
+                                    ispk = false;
+                                }
+                                (string, bool) t = (referencedColumn, ispk);
+                                
+                                var foreignKey = new ForeignKey(tableName, attributeName, referencedTable, t , isUnique);
                                 foreignKeys.Add(foreignKey);
                             }
                         }
@@ -121,7 +148,7 @@ namespace abkr.CatalogManager
             var databaseElement = new XElement("DataBase", new XAttribute("dataBaseName", databaseName));
             metadata.Add(databaseElement);
             SaveMetadata(metadata);
-            Console.WriteLine($"Database '{databaseName}' created.");
+            logger.LogMessage($"Database '{databaseName}' created.");
         }
 
         public string GetPrimaryKeyColumn(string databaseName, string tableName)
@@ -151,8 +178,7 @@ namespace abkr.CatalogManager
         public void CreateTable(
             string databaseName,
             string tableName,
-            List<Column> columns,
-            string primaryKeyColumn)
+            List<Column> columns)
         {
             // First, check the databaseName, tableName, and columns aren't null or empty
             if (string.IsNullOrEmpty(databaseName))
@@ -175,7 +201,7 @@ namespace abkr.CatalogManager
             var databaseElement = metadata.Elements("DataBase").FirstOrDefault(e => e.Attribute("dataBaseName")?.Value == databaseName)
                 ?? throw new ArgumentException($"Database '{databaseName}' does not exist.");
 
-            Console.WriteLine($"CatalogManagar.CreateTable: Creating table '{tableName}' in database '{databaseName}'.");
+            logger.LogMessage($"CatalogManagar.CreateTable: Creating table '{tableName}' in database '{databaseName}'.");
 
             // Check if the table already exists
             var tableElement = databaseElement.Descendants("Table").FirstOrDefault(e => e.Attribute("tableName").Value == tableName);
@@ -184,19 +210,17 @@ namespace abkr.CatalogManager
                 throw new ArgumentException($"Table '{tableName}' already exists in database '{databaseName}'.");
             }
 
-            Console.WriteLine("Proceeding.");
-
             // If the table doesn't exist, create a new one
             tableElement = new XElement("Table", new XAttribute("tableName", tableName));
 
 
             // Create a new structure element
             var structureElement = new XElement("Structure");
-            int i = 0;
+
             // Iterate through the provided columns
             foreach (var column in columns)
             {
-                Console.WriteLine($"CatalogManager.CreateTable: Adding column '{column.Name}' to table '{tableName}' in iteration {i++}.");
+                //logger.LogMessage($"CatalogManager.CreateTable: Adding column '{column.Name}' to table '{tableName}' in iteration {i++}.");
                 XElement attribute = new XElement("Attribute");
                 attribute.SetAttributeValue("attributeName", column.Name);
 
@@ -213,8 +237,8 @@ namespace abkr.CatalogManager
                 {
                     attribute.SetAttributeValue("isPrimaryKey", "true");
                     attribute.SetAttributeValue("isUnique", "true");
-                    Console.WriteLine($"Primary key attribute added for column: {column.Name}");
-                    Console.WriteLine($"Unique key attribute added for primary key: {column.Name}");
+                    logger.LogMessage($"Primary key attribute added for column: {column.Name}");
+                    logger.LogMessage($"Unique key attribute added for primary key: {column.Name}");
                 }
 
                 // Check if the column is a foreign key
@@ -223,7 +247,9 @@ namespace abkr.CatalogManager
                     // Split the ForeignKeyReference to get the table and column 
 
                     string foreignTable = column.ForeignKeyReference.ReferencedTable;
-                    string foreignColumn = column.ForeignKeyReference.ReferencedColumn;
+                    string foreignColumn = column.ForeignKeyReference.ReferencedColumn.Item1;
+
+                    logger.LogMessage($"Foreign key attribute added from column: {foreignColumn} in Table: {foreignTable}");
 
                     // Check if the referenced column in the foreign table is unique
                     if (!IsForeignKeyUnique(databaseName, foreignTable, foreignColumn))
@@ -243,33 +269,33 @@ namespace abkr.CatalogManager
 
 
                     attribute.SetAttributeValue("isForeignKey", "true");
-                    attribute.SetAttributeValue("references", column.ForeignKeyReference.ToString());
-                    Console.WriteLine($"Foreign key attribute added for column: {column.Name}");
+                    attribute.SetAttributeValue("references", $"{databaseName}.{foreignTable}({foreignColumn})");
+                    logger.LogMessage($"Foreign key attribute added for column: {column.Name}");
                 }
 
                 // Check if the column is a unique key
                 if (column.IsUnique)
                 {
                     attribute.SetAttributeValue("isUnique", "true");
-                    Console.WriteLine($"Unique key attribute added for column: {column.Name}");
+                    logger.LogMessage($"Unique key attribute added for column: {column.Name}");
                 }
 
                 structureElement.Add(attribute);
             }
 
-            Console.WriteLine("CatalogManager.CreateTable: Adding primary key element.");
+            logger.LogMessage("CatalogManager.CreateTable: Adding primary key element.");
 
-            if (string.IsNullOrEmpty(primaryKeyColumn))
+            if (columns.Where(c=>c.IsPrimaryKey)==null)
             {
-                throw new ArgumentNullException(nameof(primaryKeyColumn), "Primary key column cannot be null or empty.");
+                throw new ArgumentNullException("Primary key column cannot be null or empty.");
             }
 
-            var primaryKeyElement = new XElement("primaryKey", new XAttribute("name", primaryKeyColumn));
+            var primaryKeyElement = new XElement("primaryKey", new XAttribute("name", columns.Where(c => c.IsPrimaryKey).FirstOrDefault().Name));
             tableElement.Add(structureElement);
             tableElement.Add(primaryKeyElement); // Add primary key element to table
             databaseElement.Add(tableElement);
 
-            Console.WriteLine("CatalogManager.CreateTable: Saving metadata.");
+            logger.LogMessage("CatalogManager.CreateTable: Saving metadata.");
 
             SaveMetadata(metadata);
         }
